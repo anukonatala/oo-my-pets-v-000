c2ea95f9111bc04a8cbbcc7dedcbf4355f0049bd
Anusha Konatala <anu.konatala@gmail.com>
2018-07-02 13:24:36 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -46,7 +46,9 @@ def feed_fish
 end
 
 def sell_pets
+   @pets = {:fishes=>[], :dogs=>[], :cats=>[]}
 end
+
 def list_pets
   "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
 end
