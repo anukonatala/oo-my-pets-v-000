e40d0412d172bb8235096307d779f33a6d6bb1b0
Anusha Konatala <anu.konatala@gmail.com>
2018-07-02 12:50:05 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,64 @@
 class Owner
   # code goes here
+  
+  attr_accessor :pets, :name
+  attr_reader :species = "human"
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def species
+    @species 
+  end
+  
+  def say_species
+      "I am a human." unless @species == "human"
+  end
+  
+  def name
+  end
+  
+  def pets
+  end
+  
+  def buy_fish(name)
+  end
+  
+  def buy_cat(name)
+  end
+  
+  def buy_dog(name)
+  end
+  
+  def walks_dogs
+  end
+  
+  def play_with_cats
+end
+def feed_fish
+end
+
+def sell_pets
+end
+def list_pets
+end
+
+def self.all
+  @@all
+end
+
+def self.reset_all
+  @@all.clear()
+end
+
+def self.count
+  @@all.length
+end
+
+  
+  
+ 
 end
